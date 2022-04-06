96bd9360bb6da2bee63425752c33b3f2d84ae97b
zhangjianjun <wusen@126.com>
2022-04-06 14:58:58 +0800

    The second phase of playground-manager services is online: 1. Added specific course services; 2. Added resource pool information for each course; 3. Added multiple third-party account logins;

--- task/inittask.go
+++ task/inittask.go
@@ -15,19 +15,45 @@ func StopTask() {
 	toolbox.StopTask()
 }
 
-//InitYamlTask Get yaml data source
+// Clear used resource image instance resources
 func ClearInstanceTask(clInvalidInstance string) {
-	invalidTask := toolbox.NewTask("ClearInvaildResource", clInvalidInstance, handler.ClearInvaildResource)
+	invalidTask := toolbox.NewTask("ClearInvaildResource",
+		clInvalidInstance, handler.ClearInvaildResource)
 	toolbox.AddTask("ClearInvaildResource", invalidTask)
+}
+
+// Synchronized course list and chapter information
+func SyncCourseTask(syncCourse string) {
+	syncCourseTask := toolbox.NewTask("SyncCourse", syncCourse, handler.SyncCourse)
+	toolbox.AddTask("SyncCourse", syncCourseTask)
+}
+
+// Ensure that new courses can generate resource pools
+func ApplyCoursePoolTask(applyCoursePool string) {
+	applyCoursePoolTask := toolbox.NewTask("ApplyCoursePoolTask",
+		applyCoursePool, handler.ApplyCoursePoolTask)
+	toolbox.AddTask("ApplyCoursePoolTask", applyCoursePoolTask)
 }
 
 //InitTask Timing task initialization
 func InitTask() bool {
-	// Get the original yaml data
+	// Clear used resource image instance resources
 	clInvalidInstanFlag, err := beego.AppConfig.Int("crontab::cl_invalid_instances_flag")
 	if clInvalidInstanFlag == 1 && err == nil {
 		clInvalidInstance := beego.AppConfig.String("crontab::cl_invalid_instances")
 		ClearInstanceTask(clInvalidInstance)
+	}
+	// Synchronized course list and chapter information
+	syncCourseFlag, err := beego.AppConfig.Int("crontab::sync_course_flag")
+	if syncCourseFlag == 1 && err == nil {
+		syncCourse := beego.AppConfig.String("crontab::sync_course")
+		SyncCourseTask(syncCourse)
+	}
+	// Ensure that new courses can generate resource pools
+	applyCourseFlag, err := beego.AppConfig.Int("crontab::apply_course_pool_flag")
+	if applyCourseFlag == 1 && err == nil {
+		applyCoursePool := beego.AppConfig.String("crontab::apply_course_pool")
+		ApplyCoursePoolTask(applyCoursePool)
 	}
 	return true
 }
