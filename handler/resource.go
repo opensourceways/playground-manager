e32cf5ede669d160cb0899e9bcde16338d051a29
luonancom <luonancom@qq.com>
2022-06-09 10:45:14 +0800

    delete db password eny

--- handler/resource.go
+++ handler/resource.go
@@ -924,13 +924,13 @@ func AddTmplResourceList(items unstructured.Unstructured, crs CourseRes) bool {
 		}
 	}
 	if courseId != crs.CourseId {
-		logs.Error("-----course id 不同, courseId :%v, crs.CourseId:%v ", courseId, crs.CourseId)
+		logs.Error("-----course id not equel, courseId :%v, crs.CourseId:%v ", courseId, crs.CourseId)
 		time.Sleep(time.Second * 10)
 		return false
 	}
 	if len(resType) > 0 && len(resourceName) > 0 {
 		if resType != resourceName {
-			logs.Error("-----resourceName 不同, resType :%v,resourceName:%v ", resType, resourceName)
+			logs.Error("-----resourceName not equel, resType :%v,resourceName:%v ", resType, resourceName)
 			time.Sleep(time.Second * 10)
 			return false
 		}
