dc1ac73a2e54d1921e7d15b1d558181d57318abb
luonancom <luonancom@qq.com>
2022-06-09 19:38:10 +0800

    temp

--- handler/resource.go
+++ handler/resource.go
@@ -387,7 +387,7 @@ func ParseTmpl(yamlDir string, rr ReqResource, localPath string, itr *InitTmplRe
 	if common.FileExists(tmpLocalPath) {
 		DeleteFile(tmpLocalPath)
 	}
-	UnstructuredYaml(content)
+	// UnstructuredYaml(content)
 	return content
 }
 
@@ -489,7 +489,7 @@ func UnstructuredYaml(yamlData []byte) {
 		logs.Error("dec.Decode, err: ", err)
 	}
 	// Get the common metadata, and show GVK
-	// logs.Info(obj.GetName(), gvk.String())
+	logs.Info(obj.GetName(), gvk.String())
 }
 
 func GetGVRdyClient(gvk *schema.GroupVersionKind, nameSpace, resourceId string) (dr dynamic.ResourceInterface, err error) {
